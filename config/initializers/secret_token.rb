5cb1eeaf9eb91ede9329fa69c2b16d5bf74a783a
Henning Klasén <henning@hform.se>
2012-09-14 11:34:12 +0200

    A new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babysleep::Application.config.secret_token = 'a0495c7943f82ea2cc2d0bf761d9e23a9470e5e435c07d510ddbce3d927f81512518e3c3747a4f6773bccfd95f386f13b132d098b7c9a923aac0979458d4da25'
