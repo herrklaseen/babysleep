063dc0be0be097a2a9a37531f328cced941768c6
Henning Klasén <henning@hform.se>
2012-09-19 12:05:43 +0200

    User: tests for attribute presence done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password
+
+  validates :name, :presence => true
+  validates :email, :presence => true
+  validates :password, :presence => true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,19 @@ describe User do
 
   it { should be_an_instance_of(User) }
 
+  describe "name must be present" do
+	  before { @user.name = " " }
+	  it { should_not be_valid }
+	end
+
+	  describe "email must be present" do
+	  before { @user.email = " " }
+	  it { should_not be_valid }
+	end
+
+  describe "password must be present" do
+	  before { @user.password = " " }
+	  it { should_not be_valid }
+	end
+
 end
