3cdb71d5a8e48d2017433ed6b64aed16a75e0e62
Henning Klasén <henning@hform.se>
2012-09-20 12:27:37 +0200

    User: forgot to check in one file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password
 
+  before_save { |user| user.email = user.email.downcase }
+
   VALID_EMAIL_REGEX = /\A([\w+\-_]+(\.)?)+@{1}([\w\-]+\.)+[a-z\d]{2,4}\z/i
 
   validates :name, 
