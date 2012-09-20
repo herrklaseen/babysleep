e37bab396758285d2cfa070d19d037e337f97196
Henning Klasén <henning@hform.se>
2012-09-20 11:54:25 +0200

    User: tests for email format done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password
 
+  VALID_EMAIL_REGEX = /\A([\w+\-_]+(\.)?)+@{1}([\w\-]+\.)+[a-z\d]{2,4}\z/i
+
   validates :name, :presence => true, :length => { :minimum => 5, :maximum => 100 }
-  validates :email, :presence => true
+  validates :email, :presence => true, :format => { :with => VALID_EMAIL_REGEX }
   validates :password, :presence => true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,16 +10,6 @@ describe User do
 	  it { should_not be_valid }
 	end
 
-	  describe 'email must be present' do
-	  before { @user.email = ' ' }
-	  it { should_not be_valid }
-	end
-
-  describe 'password must be present' do
-	  before { @user.password = ' ' }
-	  it { should_not be_valid }
-	end
-
 	describe 'name must be longer than 4 characters' do
 	  before { @user.name = 'a' * 4 }
 	  it { should_not be_valid }
@@ -29,5 +19,38 @@ describe User do
 	  before { @user.name = 'a' * 101 }
 	  it { should_not be_valid }
 	end
+
+	describe 'email must be present' do
+	  before { @user.email = ' ' }
+	  it { should_not be_valid }
+	end
+
+	describe 'email with valid format' do
+		it 'should be valid' do
+			valid_email_addresses = %w[me@example.com me.too@example.co.uk 
+															me+too@ex-ample.com me_me@example.com]
+			valid_email_addresses.each do |valid_address|
+				@user.email = valid_address
+				@user.should be_valid
+			end
+		end
+	end
+
+	describe 'email with invalid format' do
+		it 'should not be valid' do
+			invalid_email_addresses = %w[me..@example.com me:too@example.co.uk me%too@ex_ample.com]
+			invalid_email_addresses.each do |invalid_address|
+				@user.email = invalid_address
+				@user.should_not be_valid
+			end
+		end
+	end
+
+
+  describe 'password must be present' do
+	  before { @user.password = ' ' }
+	  it { should_not be_valid }
+	end
+
 
 end
