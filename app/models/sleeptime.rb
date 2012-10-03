e2672fc19fbc31417b32d203afe2ef10fd2e912e
Henning Klasén <henning@hform.se>
2012-10-03 10:39:08 +0200

    Sleeptime: refactored model to use duck typing

--- app/models/sleeptime.rb
+++ app/models/sleeptime.rb
@@ -16,24 +16,20 @@ class Sleeptime < ActiveRecord::Base
 
   def self.parse (starttime = (DateTime.now-24.hours), endtime = DateTime.now, baby_object = nil)
     sleeptime_object = Sleeptime.new
-    
-    if (baby_object)
-      sleeptime_object.baby = baby_object
-    end
+    sleeptime_object.baby = baby_object
 
-    if (starttime.class.name == 'DateTime')
+    if (starttime.respond_to?('year'))
       sleeptime_object.start = starttime
-    end
-
-    if (sleeptime_object.start? and endtime.class.name  == 'DateTime')
-      sleeptime_object.duration = self.create_duration_from_datetime_objects(starttime, endtime)
-    end
-
-    if (starttime.class.name  == 'String' and endtime.class.name  == 'String')
-      sleeptime_object.duration = self.create_duration_from_strings(starttime, endtime)
-      if (sleeptime_object.duration > 0) 
+    else
+      begin
         sleeptime_object.start = DateTime.strptime(starttime, '%H%M')
+      rescue ArgumentError
+        sleeptime_object.start = nil
       end
+    end
+
+    if (sleeptime_object.start?)
+      sleeptime_object.duration = self.create_duration(sleeptime_object.start, endtime)
     end
 
     sleeptime_object
@@ -41,18 +37,18 @@ class Sleeptime < ActiveRecord::Base
 
   private
 
-  def self.create_duration_from_datetime_objects(starttime, endtime)
-    duration = endtime.to_i - starttime.to_i
-  end
-
-  def self.create_duration_from_strings(starttime, endtime)
-    begin 
-      start_datetime = DateTime.strptime(starttime, '%H%M')
-      end_datetime = DateTime.strptime(endtime, '%H%M')
-      duration = self.create_duration_from_datetime_objects(start_datetime, end_datetime)
-    rescue ArgumentError, NoMethodError
-      duration = 0
+  def self.create_duration(starttime, endtime)
+    if (endtime.respond_to?('year'))
+      duration = endtime.to_i - starttime.to_i
+    else
+      begin
+        endtime = DateTime.strptime(endtime, '%H%M')
+        duration = self.create_duration(starttime, endtime)
+      rescue ArgumentError
+        duration = 0
+      end
     end
+
     duration
   end
 
