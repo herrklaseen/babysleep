9996b64c714bec20cc559a7faa194a8ca8388267
Henning Klasén <henning@hform.se>
2012-10-11 11:03:17 +0200

    Closer to functional story. Rearranged js client. Using global variables to store basic user data in client.

--- app/controllers/sleeptimes_controller.rb
+++ app/controllers/sleeptimes_controller.rb
@@ -8,13 +8,16 @@ class SleeptimesController < ApplicationController
     end    
   end
 
-  def last_24h_sleeptime
-    @sleeptimes = Sleeptime.find_all_by_baby_id(params[:baby_id])
-
+  def create
+    @baby = Baby.find_by_id(params[:baby_id])
+    @sleeptime = Sleeptime.make_instance(params[:start], params[:end], @baby)
+    @sleeptime.save!
     respond_to do |format|
       format.json { render json: @sleeptimes }
-    end    
-  end
+      format.mobile {render json: @sleeptimes }
+    end
+  end    
+
 
 
 end
