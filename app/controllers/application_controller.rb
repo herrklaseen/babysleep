dd900f3a3da425c5a074a8c0063ebe869e4844f1
Henning Klasén <henning@hform.se>
2012-11-02 12:41:17 +0100

    Preparing controllers to report user time zone.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter do 
+    set_user_time_zone
+  end
+
+  helper_method :set_user_time_zone
+
+  private
+
+  def set_user_time_zone
+    if (session[:user_time_zone])
+      Time.zone = session[:user_time_zone]
+    else
+      Time.zone = config.user_time_zone
+    end
+  end
+
+
 end
